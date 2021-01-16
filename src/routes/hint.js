83f8bd46bd2ea23949f883b2b7f8f923a4da0c8d
= <=>
2021-01-16 20:46:24 +0100

    remove consoles

--- src/routes/hint.js
+++ src/routes/hint.js
@@ -11,7 +11,7 @@ router.get("/", async ({}, res) => {
 router.get("/:id", async ({ params: { id } }, res) => {
   let hint = await Hint.findOne({ where: { id } });
   if (!hint) {
-    return res.send({ type: "error", msg: "Doesn't exist" });
+    return res.send({ type: "error", msg: "Doesn't exist" }); 
   }
   return res.send({ type: "success", data: hint });
 });
